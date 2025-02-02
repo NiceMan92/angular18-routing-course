0cc2e22c4bf0f2507ab5972b22bac5e4d0c8ae38
Abdelkrim Haddadi <abdelkrim.haddadi@gmail.com>
2025-02-02 13:02:28 +0100

    add effect example

--- src/app/setting/setting.component.ts
+++ src/app/setting/setting.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal, untracked } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,11 +8,29 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <p>
       setting works!
+      {{ double() }}
       <router-outlet />
     </p>
   `,
   styles: ``
 })
 export default class SettingComponent {
+
+   n = signal(1);
+   double = signal(0)
+
+  constructor(){
+
+    effect(() => {
+      //this.n();
+      untracked(() => {
+        this.double.set(this.n() * 2 );
+        console.log(this.double());
+      })
+
+    });
+
+    setTimeout(() => this.n.set(10))
+  }
 
 }
